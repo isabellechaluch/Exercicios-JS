4a22f5a059712d678ef70308f25786bf2b4e5835
Isabelle C Haluch <isabellechaluch8@gmail.com>
2023-06-21 18:14:03 -0300

    Initial commit

---
+++ ifs-aninhados.js
@@ -0,0 +1,20 @@
+var idade = 78
+console.log('Você tem ' + idade + ' anos.')
+if (idade < 16) {
+    console.log('Não pode votar')
+} else if (idade < 18 || idade > 65) {
+        console.log('Voto opcional')
+} else {
+    console.log('Voto obrigatório')
+}
+
+var agora = new Date()
+var hora = agora.getHours()
+console.log('Agora são ' + hora + ' horas')
+if (hora < 12) {
+    console.log('Bom dia!')
+} else if (hora < 18) {
+    console.log('Boa tarde!')
+} else {
+    console.log('Boa noite!')
+}
