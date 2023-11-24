11c62e5c1472b587a2982b99611bb988446d8124
Isabelle C Haluch <isabellechaluch8@gmail.com>
2023-11-24 18:48:11 -0300

    to do list incompleto

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -1,52 +1,72 @@
 let addTaskButton = document.querySelector('#button_add_form');
 let showTasks = document.querySelector('#show_box');
 let form = document.querySelector('form');
+let taskBoxes = document.querySelectorAll('.task_box');
+let isDragging = false;
+let setX, setY, dragTask;
+//Criar a div da task
+function createDiv () {
+    let div = document.createElement('div');
+    div.classList.add('task_box');
+    return div;
+}
 
-function addTask () {
-    let task = document.querySelector('#task_form').value;
-    let newTask;
-    let textTask;
-    let buttonsBox;
-    let checkTask;
-    let removeTask;
-
-    if (task === '') return; //Se o campo da task estiver vazio, encerra a função
-
-    // Criar a task
-    newTask = document.createElement('div');
-    newTask.classList.add('show_box');
-
-    //Criar <p> para o texto da task
-    textTask = document.createElement('p');
+//Criar <p> para o texto da task
+function createText (text) { //Recebe o parâmetro texto da task
+    let textTask = document.createElement('p');
     textTask.classList.add('text-box');
-    textTask.textContent = task;
-    
-    //Criar div dos botoes
-    buttonsBox = document.createElement('div');
+    textTask.textContent = text;
+    return textTask;
+}
 
-    // Criar botão para riscar task
-    checkTask = document.createElement('button');
+//Criar div dos botões
+function createButtonsDiv () {
+    let buttonsBox = document.createElement('div');
+    return buttonsBox;
+}
+
+//Criar botão de riscar tarefa
+function createCheckButton () {
+    let checkTask = document.createElement('button');
     checkTask.textContent = 'Finished';
     checkTask.classList.add('check-button');
     checkTask.id = 'button_check';
+    return checkTask;
+}
 
-    //Criar botao para excluir task
-    removeTask = document.createElement('button');
+//Criar botao para excluir task
+function createDeleteButton () {
+    let removeTask = document.createElement('button');
     removeTask.textContent = 'Remove';
     removeTask.classList.add('remove-task-button')
     removeTask.id = 'button_remove';
+    return removeTask;
+}
 
-    //Adicionar elementos criados
-    showTasks.appendChild(newTask);
-    newTask.appendChild(textTask);
-    newTask.appendChild(buttonsBox);
-    buttonsBox.appendChild(checkTask);
-    buttonsBox.appendChild(removeTask);
+//Cria a tarefa
+function addTask () {
+    let task = document.querySelector('#task_form').value;
 
-    form.reset();
+    if (task === '') return; //Se a tarefa estiver vazia, encerra a função
 
-    checkTask.addEventListener('click', finishTask);
-    removeTask.addEventListener('click', deleteTask);
+    //Chamada das funções para criar elementos
+    let newDiv = createDiv();
+    let newText = createText(task);
+    let newButtonsbox = createButtonsDiv();
+    let newCheckButton = createCheckButton();
+    let newDelButton = createDeleteButton();
+
+   //Adicionar elementos criados
+   showTasks.appendChild(newDiv);
+   newDiv.appendChild(newText);
+   newDiv.appendChild(newButtonsbox);
+   newButtonsbox.appendChild(newCheckButton);
+   newButtonsbox.appendChild(newDelButton);
+
+   form.reset();
+   
+   newCheckButton.addEventListener('click', finishTask);
+   newDelButton.addEventListener('click', deleteTask);
 }
 
 function finishTask () {
@@ -60,5 +80,30 @@ function deleteTask () {
     //this se refere ao botao e parentelement.parentElement ao elemento pai do seu elemento pai 
     //remove o elemento filho selecionado anteriormente do elemento showTasks
 }
+
+//Arrastar e soltar as tasks
+
+taskBoxes.forEach(taskBox => {
+    taskBox.addEventListener('mousedown', (e) => {
+        isDragging = true;
+        dragTask = taskBox;
+        setX = e.clientX - taskBox.getBoundingClientRect().left;
+        setY = e.clientY - taskBox.getBoundingClientRect().top;
+    });
+});
+
+
+document.addEventListener('mousemove', (e) => {
+    if (isDragging) {
+        const x = e.clientX - setX;
+        const y = e.clientY - setY;
+        taskBoxes.style.left = `${x}px`;
+        taskBoxes.style.top = `${y}px`;
+    }
+});
+
+document.addEventListener('mouseup', () => {
+    isDragging = false;
+});
 
 addTaskButton.addEventListener('click', addTask);
